f6b02f741fd9d4fd71a94155b1cbc661590473e9
M-Hammad-Ali <mohammadhammad0358@gmail.com>
2020-09-24 04:50:06 -0700

    Google Pay Integration

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import '@google-pay/button-element';
+
+declare var google: any;
+
+import { ReadyToPayChangeResponse } from '@google-pay/button-element';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  amount = '100.00';
+  buttonType = 'buy';
+  buttonColor = 'default';
+  existingPaymentMethodRequired = false;
+
+  paymentRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId',
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant',
+    },
+  };
+
+  onLoadPaymentData = (event: CustomEvent<google.payments.api.PaymentData>): void => {
+    console.log('load payment data', event.detail);
+  };
+
+  onError = (event: ErrorEvent): void => {
+    console.error('error', event.error);
+  };
+
+  onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = paymentData => {
+    console.log('payment authorized', paymentData);
+
+    return {
+      transactionState: 'SUCCESS',
+    };
+  };
+
+  onReadyToPayChange = (event: CustomEvent<ReadyToPayChangeResponse>): void => {
+    console.log('ready to pay change', event.detail);
+  };
+}
